cc39df22376f64f78f3f2d25d4d23f69e4772428
wagmeal <kowatari19960531@gmail.com>
2026-02-16 21:38:29 +0900

    Initial Commit

---
+++ PairFund/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PairFund
+//
+//  Created by takumi kowatari on 2026/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PairFund/PairFundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PairFundApp.swift
+//  PairFund
+//
+//  Created by takumi kowatari on 2026/02/16.
+//
+
+import SwiftUI
+
+@main
+struct PairFundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
